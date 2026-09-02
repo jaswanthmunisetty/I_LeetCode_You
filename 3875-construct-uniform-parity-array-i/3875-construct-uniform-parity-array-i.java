1671dcca16308ea2c17810ee4b7bf89d92008b83
jaswanthmunisetty <137399841+jaswanthmunisetty@users.noreply.github.com>
2026-09-02 11:28:47 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -2,18 +2,5 @@ class Solution {
     public boolean uniformArray(int[] nums) {
         return true;
         // nee yabba sanka naaka kadhara solve cheyaniki, return true endhi ra ungamma, modda lo leetcode!
-        // int count = 0;
-
-        // for (int num : nums) {
-        //     if (num % 2 != 0) {
-        //         count++;
-        //     }
-        // }
-
-        // if (count == 0 || count >= 2) {
-        //     return true;
-        // }
-
-        // return false;
     }
 }
